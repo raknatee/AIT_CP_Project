305c705d33f1947b284eb930703731c9912968aa
Raknatee <raknatee@gmail.com>
2020-11-11 15:32:44 +0700

    add mapping_matrix

---
+++ code/001_get_start_with_code/my_modules/mapping_matrix.py
@@ -0,0 +1,13 @@
+ii = -1
+
+eeg_channels = [
+    [ii,ii,00,ii,ii,ii,16,ii,ii],
+    [ii,ii,01,ii,ii,ii,17,ii,ii],
+    [03,ii,02,ii,18,ii,19,ii,20],
+    [ii,04,ii,05,ii,22,ii,21,ii],
+    [07,ii,06,ii,23,ii,24,ii,25],
+    [ii,08,ii,09,ii,27,ii,26,ii],
+    [11,ii,10,ii,15,ii,28,ii,29],
+    [ii,ii,12,ii,ii,ii,30,ii,ii],
+    [ii,ii,13,ii,14,ii,31,ii,ii]
+]
